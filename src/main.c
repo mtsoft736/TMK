bfe53418bef1c6e9e8948f0b37576c898b5633ef
Matěj Truxa (M.T. Software) <matej.truxa@re-labs.eu>
2026-06-20 16:20:54 +0200

    writechar32() fn testing if it will work

--- src/main.c
+++ src/main.c
@@ -2,13 +2,16 @@
 void writechar32() {
 	char *p_vga_memmory_address = 0xB8000;
 
+	p_vga_memmory_address[0] = 'A';
+	p_vga_memmory_address[0] = 0x0F42;
+
 }
 
 
 //main fn.
 void kernel_main(void) {
 
-writechar32("A");
+writechar32();
 
 
 }
